bd4dfffaf83fa46fd07e10b8a499ac3f359b5347
Bimsara Hirushan <48193447+bimsaram997@users.noreply.github.com>
2024-04-27 11:53:15 +0300

    Login functionality added

--- frontend/frontend-app/src/App.jsx
+++ frontend/frontend-app/src/App.jsx
@@ -1,17 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { ListSandwiches } from "./components/ListSandwiches.jsx";
 import { ListOrders } from "./components/ListOrders.jsx";
-
+import {AuthUser} from "./components/AuthUser.jsx";
 const url = "http://localhost:3001/api/v1";
 
 const App = () => {
   const [sandwiches, setSandwiches] = useState([]);
   const [orders, setOrders] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const usernameRef = useRef(''); // Declare username as a ref
+  const passwordRef = useRef('');
 
   useEffect(() => {
     fetchAllSandwiches();
     fetchAllOrders();
-  }, []);
+  }, [isLoggedIn]);
 
   const fetchAllSandwiches = async () => {
     try {
@@ -43,10 +46,76 @@ const App = () => {
     }
   };
 
+  const register = async (username, password) => {
+    try {
+   
+      // Make an API request to register a new user
+      const response = await fetch(url + '/user', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+      
+
+      // Assign extracted values to refs
+      usernameRef.current = data.username;
+      passwordRef.current = password;
+      setIsLoggedIn(true);
+    } catch (error) {
+      console.error(error);
+      
+    }
+  };
+
+  
+  const login = async (username, password) => {
+    try {
+      // Make an API request to fetch players with credentials for login
+      const response = await fetch(url + '/user/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+        credentials: 'include'
+      });
+      const data = await response.json();
+      if (data.message == 'User does not exist' || data.message == 'Incorrect password') {
+        setIsLoggedIn(false);
+      } else{
+        usernameRef.current = username;
+        passwordRef.current = password;
+       
+        setIsLoggedIn(true);
+      }
+      
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+  const logout = () => {
+    // Reset app state on logout
+    setIsLoggedIn(false);
+    // setPlayers([]);
+    // setSelectedPlayer(null);
+  };
+
   return (
     <div>
-      <ListSandwiches sandwiches={sandwiches} />
+      <AuthUser isLoggedIn={isLoggedIn} onLogin={login} onRegister={register} onLogout={logout}/>
+      
+      {isLoggedIn && (
+        <div>
+          <ListSandwiches sandwiches={sandwiches} />
       <ListOrders orders={orders} />
+        </div>
+      )}
     </div>
   );
 };
